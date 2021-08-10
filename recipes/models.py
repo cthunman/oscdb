24688bca8ff076fc7b000333518fab8a2d9badca
Carl Thunman <thunman@gmail.com>
2021-08-09 21:11:36 -0400

    creating a couple stub models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    MEASUREMENT_UNITS = (
+        ('V', 'Volume'),
+        ('W', 'Weight'),
+    )
+    name = models.CharField(max_length=200)
+    measurement_units = models.CharField(max_length=1, choices=MEASUREMENT_UNITS)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
