1cb283e7e6345cac689ab6f3c482c7fe75aedda7
Carl Thunman <thunman@gmail.com>
2021-08-24 19:30:32 -0400

    tinkering more with the model... thinking about units

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from quantityfield.fields import QuantityField
 
 # Create your models here.
 class Ingredient(models.Model):
@@ -9,6 +10,10 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=200)
     measurement_units = models.CharField(max_length=1, choices=MEASUREMENT_UNITS)
 
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient)
+    quantity = QuantityField('kilogram')
 
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
+    instructions = models.TextField()
