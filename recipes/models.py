f57c8a9f583b41e63841029d527fbddcad7d88f4
Carl Thunman <thunman@gmail.com>
2021-09-14 23:07:48 -0400

    adding rudimentary units model... not sophisticated but I think we do not get a huge benefit from an overbuilt units configuration

--- recipes/models.py
+++ recipes/models.py
@@ -1,19 +1,25 @@
 from django.db import models
-from quantityfield.fields import QuantityField
 
 # Create your models here.
 class Ingredient(models.Model):
-    MEASUREMENT_UNITS = (
-        ('V', 'Volume'),
-        ('W', 'Weight'),
-    )
     name = models.CharField(max_length=200)
-    measurement_units = models.CharField(max_length=1, choices=MEASUREMENT_UNITS)
-
-class RecipeIngredient(models.Model):
-    ingredient = models.ForeignKey(Ingredient)
-    quantity = QuantityField('kilogram')
 
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
     instructions = models.TextField()
+
+class RecipeIngredient(models.Model):
+    class Units(models.TextChoices):
+        GRAM = 'G', 'Gram'
+        KILOGRAM = 'KG', 'Kilogram'
+        INCH = 'IN', 'Inch'
+        TABLESPOON = 'TBSP', 'Tablespoon'
+        TEASPOON = 'TSP', 'Teaspoon'
+        CUP = 'CUP', 'Cup'
+        OUNCE_VOLUME = 'OZ_V', 'Ounce'
+        OUNCE_MASS = 'OZ_M', 'Ounce'
+
+    ingredient = models.ForeignKey(Ingredient)
+    quantity = models.DecimalField(max_digits=8, decimal_places=3)
+    unit = models.CharField(max_length=4, choices=Units.choices)
+    recipe = models.ForeignKey(Recipe)
